88454763ead09fc7492555ab9c606f6f47e4a409
Hamed Razizadeh <hamed.razizadeh@gmail.com>
2026-01-21 19:40:51 +0100

    completed week 2 assignment task 1

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -1,6 +1,22 @@
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 const prompt = promptSync();
 
+const yearInput = prompt("Enter a year:");
+const year = Number(yearInput);
+
+if (!Number.isInteger(year) || year < 1 || year > 9999) {
+  console.log("Invalid year!");
+} else {
+  if (year % 400 === 0) {
+    console.log(`Yes, ${year} is a leap year`);
+  } else if (year % 100 === 0) {
+    console.log(`No, ${year} is not a leap year`);
+  } else if (year % 4 === 0) {
+    console.log(`Yes, ${year} is a leap year`);
+  } else {
+    console.log(`No, ${year} is not a leap year`);
+  }
+}
 
 // Write your code here
 // Guidance:
